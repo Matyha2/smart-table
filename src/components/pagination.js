794022f56f2a417ffc5ad78dae0da26002d4d252
Matvey <matpalion@mail.ru>
2025-10-16 09:00:14 -0200

    1 коммит реализация функционала

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,18 +1,49 @@
-import {getPages} from "../lib/utils.js";
+import { getPages } from "../lib/utils.js";
 
-export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
-    // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+export const initPagination = (
+  { pages, fromRow, toRow, totalRows },
+  createPage
+) => {
+  // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+  const pageTemplate = pages.firstElementChild.cloneNode(true);
+  pages.firstElementChild.remove();
 
-    return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+  let pageCount;
 
-        // @todo: #2.6 — обработать действия
+  const applyPagination = (query, state, action) => {
+    const limit = state.rowsPerPage;
+    let page = state.page;
 
-        // @todo: #2.4 — получить список видимых страниц и вывести их
+    // переносим код, который делали под @todo: #2.6
 
-        // @todo: #2.5 — обновить статус пагинации
+    return Object.assign({}, query, {
+      // добавим параметры к query, но не изменяем исходный объект
+      limit,
+      page,
+    });
+  };
 
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        return data.slice(0, 10);
-    }
-}
+  const updatePagination = (total, { page, limit }) => {
+    pageCount = Math.ceil(total / limit);
+
+    // @todo: #2.4 — получить список видимых страниц и вывести их
+    const visiblePages = getPages(page, pageCount, 5);
+    pages.replaceChildren(
+      ...visiblePages.map((pageNumber) => {
+        const el = pageTemplate.cloneNode(true);
+        return createPage(el, pageNumber, pageNumber === page);
+      })
+    );
+
+    // @todo: #2.5 — обновить статус пагинации (обратите внимание, что rowsPerPage заменена на limit)
+    const skip = (page - 1) * limit;
+    fromRow.textContent = skip + 1;
+    toRow.textContent = Math.min(skip + limit, total);
+    totalRows.textContent = total;
+  };
+
+  return {
+    updatePagination,
+    applyPagination,
+  };
+};
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,37 @@
 import {sortCollection, sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
+    return (query, state, action) => {
         let field = null;
         let order = null;
 
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
 
             // @todo: #3.2 — сбросить сортировки остальных колонок
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
         } else {
             // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
+        } if (field && order && order !== 'none') {
+            return Object.assign({}, query, {
+                sort: field,
+                order: order
+            });
         }
 
-        return sortCollection(data, field, order);
+         return query;
     }
 }
